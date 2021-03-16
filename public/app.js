f724ef31b667daeff6eb1d184128b261b69528a8
Wesley Coleman <77476122+wesleyjay09@users.noreply.github.com>
2021-03-16 16:15:56 -0500

    first fetch

---
+++ public/app.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById("button")
+const container = document.querySelector(".container")
+btn.addEventListener("click",  async ()=>{
+    fetch('http://localhost:8000/api/task')
+    .then(async(result)=> {
+        let text = await result.text();
+        container.innerHTML = text;
+    })
+})
