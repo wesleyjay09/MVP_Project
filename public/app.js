91f3b4e97c27012e09e32066cfe22c3edd6e6719
Wesley Coleman <77476122+wesleyjay09@users.noreply.github.com>
2021-03-17 14:57:37 -0500

    update

--- public/app.js
+++ public/app.js
@@ -17,10 +17,11 @@ const get = async () => {
 const post = () => {
     fetch('https://quiet-basin-82276.herokuapp.com/api/task', {
         method: 'POST',
-        body: JSON.stringify(input.value),
+        body: (input.value),
         headers: {"Content-type": "application/json; charset=UTF-8"}
     })
     .then(response => response.json()) 
+    .catch(err => err.message)
 
 
 }
