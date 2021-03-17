1a1acc259b7c4987550cc136afd5d1c4e1fb9017
Wesley Coleman <77476122+wesleyjay09@users.noreply.github.com>
2021-03-17 15:40:44 -0500

    update

--- public/app.js
+++ public/app.js
@@ -24,7 +24,7 @@ const post = () => {
     })
     .then(async (result) => {
         let text = await result.text()
-        container.innterHTML = text;\q
+        container.innterHTML = text;
     
     
     })
