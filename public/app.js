8a39b26c3ccd5a6555a20c4c57333d8980ce38d8
Wesley Coleman <77476122+wesleyjay09@users.noreply.github.com>
2021-03-17 14:50:34 -0500

    update

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@ const submit = document.querySelector("#submit")
 
 
 
-const get = async ()=>{
+const get = async () => {
     fetch('https://quiet-basin-82276.herokuapp.com/api/task')
     .then(async(result)=> {
         let text = await result.text();
@@ -14,9 +14,20 @@ const get = async ()=>{
     })
 }
 
+const post = () => {
+    fetch('https://quiet-basin-82276.herokuapp.com/api/task', {
+        method: 'POST',
+        body: JSON.stringify(input.value),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    .then(response => response.json()) 
+
+
+}
 
 
 
 
 
+submit.addEventListener("click", post())
 get()
