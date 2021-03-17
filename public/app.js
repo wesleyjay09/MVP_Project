2596655390d1fe4aafb5358e75524ad9534dcc5c
Wesley Coleman <77476122+wesleyjay09@users.noreply.github.com>
2021-03-17 09:17:26 -0500

    update

--- public/app.js
+++ public/app.js
@@ -7,8 +7,8 @@ const get = async ()=>{
     fetch('https://quiet-basin-82276.herokuapp.com/api/task')
     .then(async(result)=> {
         let text = await result.text();
-        console.log(JSON.stringify(text))
-        container.innerHTML = text[i];
+        console.log(JSON.parse(text))
+        container.innerHTML = text[0];
         
         
     })
