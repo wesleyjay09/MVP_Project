472fe673e5be3262afc7e9f2574a4ed212b8ed33
Wesley Coleman <77476122+wesleyjay09@users.noreply.github.com>
2021-03-17 18:22:44 -0500

    update

--- public/app.js
+++ public/app.js
@@ -17,7 +17,7 @@ const get = () => {
         // var li = document.createElement('li');
         // li.textContent = text;
         // list.appendChild(li);
-   console.log(JSONparse(text))
+   console.log(JSON.parse(text))
     
     })
 }
