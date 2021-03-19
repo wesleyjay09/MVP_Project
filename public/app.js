904e6071ebfe4696cb767ffe8d705f374fe31eb2
Wesley Coleman <77476122+wesleyjay09@users.noreply.github.com>
2021-03-19 01:30:18 -0500

    update

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 
-
+//selectors
 const listContainer = document.getElementById("listContainer")
 const input = document.getElementById("input");
 const submit = document.querySelector("#submit")
@@ -13,6 +13,8 @@ check.setAttribute("type", "checkbox");
 let userInput;
 let deleteInput;
 
+
+//fetch
 const get = () => {
     fetch('https://quiet-basin-82276.herokuapp.com/api/todo')
     .then(async(result)=> {
@@ -67,34 +69,23 @@ const put = () => {
        })
 
 }
-
+//modal variables
 const modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
 const btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
+ btn.onclick = function() {
   modal.style.display = "block";
 }
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
-
-
- 
 empty.addEventListener("click", get)
 //submit.addEventListener("click", put)
 submit.addEventListener("click", post)
