3483fa76727e87805491c2255088ffe2943c3000
Wesley Coleman <77476122+wesleyjay09@users.noreply.github.com>
2021-03-19 01:12:46 -0500

    update

--- public/app.js
+++ public/app.js
@@ -7,10 +7,8 @@ const button = document.getElementById("button")
 const update = document.getElementById("update")
 const deleteBtn = document.getElementById("delete")
 const empty = document.getElementById("empty")
-
 let check = document.createElement("input")
 check.setAttribute("id" ,"checked")
-
 check.setAttribute("type", "checkbox");
 let userInput;
 let deleteInput;
@@ -42,8 +40,6 @@ const get = () => {
     })
 }
 
-
-
 const post = () => {
     fetch('https://quiet-basin-82276.herokuapp.com/api/todo', {
         method: 'POST',
@@ -72,7 +68,30 @@ const put = () => {
 
 }
 
+const modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+const btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 
  
